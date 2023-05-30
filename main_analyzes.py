f9dab61e367cf24f5e815699b1be2bb3671692b2
Euler Magno <eulermagno.mstrg@gmail.com>
2023-05-30 09:07:12 -0300

    Update main_analyzes.py

--- main_analyzes.py
+++ main_analyzes.py
@@ -2,6 +2,17 @@
 import pandas as pd
 
 # Importando a base de dados
-tabela_geral =pd.read_csv('Minicurso Análise de Dados no Python/ClientesBanco.csv', encoding = 'latin1')
+# Necessário inserir o encoding pelos caracteres que contém no ClientesBanco.csv
+tabela_geral = pd.read_csv('Minicurso Análise de Dados no Python/ClientesBanco.csv', encoding='latin1')
+
+# Tratamento da base de dados
+print(f'{tabela_geral}\n')
 pd.set_option('display.max_columns', None)
-print(tabela_geral)
+tabela_geral = tabela_geral.drop('CLIENTNM', axis=1)
+
+
+
+
+
+
+
